ec16f5425d2f8e4d1814828666b336576a70a7e1
Rafael Galafassi <93929600+Tagliassi@users.noreply.github.com>
2023-03-28 21:13:36 -0300

    RA - 28_03
    Exercícios realizados com estruturas de controle, como if.

---
+++ exercicios_02.py
@@ -0,0 +1,17 @@
+#Buscando informação da média tirada pelo auno na matéria
+#media = float(input("Digite a sua média em determinada matéria:"))
+#Se o valor da média for igual ou maior a 7.0 o aluno está aprovado
+#if media >= 7.0:
+#    print("aprovado")
+
+#Buscando a informação de determinada conta corrente:
+#saldo_conta_corrente = float(input("Digite o saldo da sua conta bancária:"))
+#if saldo_conta_corrente > 0:
+    #print("Positivo")
+#if saldo_conta_corrente < 0:
+    #print("Negativo", saldo_conta_corrente)
+    
+#Buscando a informação da idade de uma pessoa e se ela pode dirigir:
+idade = int(input("Digite a sua idade:"))
+if idade >= 18:
+    print("apto a dirigir")
