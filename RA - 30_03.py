56f965695a71467feb156d59f231a2697468a082
Rafael Galafassi <93929600+Tagliassi@users.noreply.github.com>
2023-03-30 21:10:03 -0300

    RA - 30_03
    Exercicios de if, elif e else.

---
+++ RA - 30_03.py
@@ -0,0 +1,34 @@
+#Definindo o número de caixas, livros e o seu peso.
+#numeroCaixas = int(input("Quantas caixas o caminhão pode transportar:" ))
+#numeroLivros = int(input("Quantos livros pode ter em cada caixa:" ))
+#pesoLivro = float(input("Qual é o peso de um livro:" ))
+#Realizando o cálculo da quantidade de livros transportados pelo caminhão e o peso total da carga.
+#livrosCaminhao = numeroLivros * numeroCaixas
+#cargaTotal = numeroLivros * pesoLivro
+#Mostrando na tela o resultado do cálculo.
+#print("Quantidade de livros transportadas pelo caminhão é igual a:", livrosCaminhao)
+#print("Quantidade de carga transportada pelo caminhão em quilos é igual a:", cargaTotal)
+
+#qtdeCopias = int(input("Quantas copias foram realizadas:"))
+#custoTotal = float(input("Qual o custo total da impressão:"))
+#valorCopia = custoTotal/qtdeCopias
+#print("O valor pago por copia é igual a:", valorCopia )
+
+#idade = int(input("Qual a sua idade:" ))
+#idadeMeses = idade*12
+#idadePermitida = 18*12
+#if idade >= 18:
+    #print("Pode dirigir e votar")
+#else:
+    #mesesParaDirigir = idadePermitida - idadeMeses
+    #print("Não pode dirigir ainda, falta", mesesParaDirigir, "meses para poder dirigir")
+
+#idade = int(input("Qual a sua idade:"))
+#if idade <16:
+    #print("Não pode votar e dirigir")
+#elif idade >= 16 and idade <18:
+    #print("Pode votar e não pode dirigir")
+#else:
+    #print("Pode votar e dirigir")
+#print("Fim do programa!")
+
