2720edca23ee1a875d8acd266be268393f137d98
Tagliassi <rafael.galafassi2005@gmail.com>
2023-04-06 18:44:36 -0300

    RA

---
+++ Raciocinio-Algoritimico/Exercise_04.py
@@ -0,0 +1,13 @@
+# 01:
+# contador = 1
+# soma = 0
+# while contador < 11:
+# soma = soma + contador
+# print("somatório =", soma)
+# contador = contador + 1
+
+# 02:
+nota = float(input("Digite a nota (entre 0 e 10) = "))
+while nota < 0 or nota > 10:
+    print("Nota invalida. Digite novamente.")
+    nota = float(input("Digite a nota (entre 0 e 10) = "))
