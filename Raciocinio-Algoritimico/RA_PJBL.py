598f7102bc42922fa638e7017e81e0e5abf3bb5c
Tagliassi <rafael.galafassi2005@gmail.com>
2023-04-12 15:10:02 -0300

    RA

--- Raciocinio-Algoritimico/RA_PJBL.py
+++ Raciocinio-Algoritimico/RA_PJBL.py
@@ -12,6 +12,7 @@ valorCompra = 0
 totalCedulas = 0
 totalMoedas = 0
 novoTotalCedulas = 0
+senha = ""
 
 # inicializa o loop continuo do programa
 while True:
@@ -34,96 +35,95 @@ while True:
     # Se o usuário for administrador habilita as funcionalidades para gerenciar a máquina
     if usuario == "1":
         # solicita a senha para habilitar o sistema
-        senha = int(input("Digite a senha para habilitar o sistema: "))
-        if senha == 1234:
-            # Exibe as opções disponíveis para o administrador
-            print("------------------------------------------------------------")
-            opcaoAdm = input(
-                "Selecione a opção:\n(1) para atualizar o troco na máquina\n(2) para atualizar o estoque de produtos\n(3) para ver a quantidade de dinheiro das vendas\n")
-            # Se a opção selecionada for equivalente a 1, o troco será atualizado por tipo e quantidade.
-            if opcaoAdm == "1":
-                # Solicita a quantidade de cédulas de cada valor e atualiza o total em dinheiro da máquina
-                print("------------------------------------------------------------")
-                print("Digite a quantidade de cédulas")
-                qtdeCedulas2 = int(
-                    input("Digite a quantidade de notas de 2: "))
-                qtdeCedulas5 = int(
-                    input("Digite a quantidade de notas de 5: "))
-                qtdeCedulas10 = int(
-                    input("Digite a quantidade de notas de 10: "))
-                qtdeCedulas20 = int(
-                    input("Digite a quantidade de notas de 20: "))
-                novoTotalCedulas = (qtdeCedulas2*2) + (qtdeCedulas5*5) + \
-                    (qtdeCedulas10*10) + (qtdeCedulas20*20)
-                totalCedulas = novoTotalCedulas
-                print(
-                    f"A quantidade de cédulas de 2 reais é de: {qtdeCedulas2}")
-                print(
-                    f"A quantidade de cédulas de 5 reais é de: {qtdeCedulas5}")
-                print(
-                    f"A quantidade de cédulas de 10 reais é de: {qtdeCedulas10}")
-                print(
-                    f"A quantidade de cédulas de 20 reais é de: {qtdeCedulas20}")
-                print(
-                    f"O total de dinheiro em cedulas na máquina é de R${totalCedulas}")
-                # Solicita a quantidade de moedas de cada valor e atualiza o total em dinheiro da máquina
-                print("------------------------------------------------------------")
-                print("Digite a quantidade de moedas")
-                qtdeMoedas5 = int(
-                    input("Digite a quantidade de moedas de 5 centavos: "))
-                qtdeMoedas10 = int(
-                    input("Digite a quantidade de moedas de 10 centavos: "))
-                qtdeMoedas25 = int(
-                    input("Digite a quantidade de moedas de 25 centavos: "))
-                qtdeMoedas50 = int(
-                    input("Digite a quantidade de moedas de 50 centavos: "))
-                qtdeMoedas1 = int(
-                    input("Digite a quantidade de moedas de 1 real: "))
-                novoTotalMoedas = ((qtdeMoedas5*5)/100) + ((qtdeMoedas10*10)/100) + \
-                    ((qtdeMoedas25*25)/100) + \
-                    ((qtdeMoedas50*50)/100) + (qtdeMoedas1*1)
-                totalMoedas = novoTotalMoedas
-                print(
-                    f"A quantidade de moedas de 5 centavos é de: {qtdeMoedas5}")
-                print(
-                    f"A quantidade de moedas de 10 centavos é de: {qtdeMoedas10}")
-                print(
-                    f"A quantidade de moedas de 25 centavos é de: {qtdeMoedas25}")
-                print(
-                    f"A quantidade de moedas de 50 centavos é de: {qtdeMoedas50}")
-                print(f"A quantidade de moedas de 1 real é de: {qtdeMoedas1}")
-                print(
-                    f"O total de dinheiro em moedas na máquina é de R${totalMoedas:.2f}")
-                # Atualiza a quantidade de troco disponível na máquina
-                print("------------------------------------------------------------")
-                novo_troco = (totalCedulas + totalMoedas)
-                print(f"O total de troco na máquina é de R${novo_troco:.2f}")
-                troco = novo_troco
-                print("Troco atualizado com sucesso!")
-                print("------------------------------------------------------------")
-            # Se a opção selecionada for equivalente a 2, o estoque será atualizado por tipo e quantidade de produtos disponíveis.
-            elif opcaoAdm == "2":
-                print("------------------------------------------------------------")
-                print(
-                    "A quantidade de unidade(s) de Coca-Cola atual em estoque é igual a: ", qtdeProduto1)
-                print("------------------------------------------------------------")
-                novaQtdeProduto1 = int(
-                    input("Adicione mais unidade(s) de Coca-Cola ao estoque: "))
-                qtdeProduto1 = novaQtdeProduto1
-                print("Estoque atualizado com sucesso!")
-                print("------------------------------------------------------------")
-            # Verifica o valor recebido das vendas por produto e valor total durante o funcionamento da máquina
-            elif opcaoAdm == "3":
-                print("O valor recebido das vendas de Coca-Cola é igual a: R$",
-                      (qtdeProduto1Vendido*4))
-                print("O valor total das vendas é igual a: R$",
-                      (qtdeProduto1Vendido*4))
-            # Verifica se usuario selecionou uma opção válida de entrada, caso contrário retorna ao início do loop
-            else:
-                print("Opção inválida, tente novamente")
-                continue
-        else:
+        senha = input("Digite a senha para habilitar o sistema: ")
+        while senha != "1234":
             print("Senha inválida, tente novamente")
+            senha = input("Digite a senha para habilitar o sistema: ")
+        # Exibe as opções disponíveis para o administrador
+        print("------------------------------------------------------------")
+        opcaoAdm = input(
+            "Selecione a opção:\n(1) para atualizar o troco na máquina\n(2) para atualizar o estoque de produtos\n(3) para ver a quantidade de dinheiro das vendas\n")
+        # Se a opção selecionada for equivalente a 1, o troco será atualizado por tipo e quantidade.
+        if opcaoAdm == "1":
+            # Solicita a quantidade de cédulas de cada valor e atualiza o total em dinheiro da máquina
+            print("------------------------------------------------------------")
+            print("Digite a quantidade de cédulas")
+            qtdeCedulas2 = int(
+                input("Digite a quantidade de notas de 2: "))
+            qtdeCedulas5 = int(
+                input("Digite a quantidade de notas de 5: "))
+            qtdeCedulas10 = int(
+                input("Digite a quantidade de notas de 10: "))
+            qtdeCedulas20 = int(
+                input("Digite a quantidade de notas de 20: "))
+            novoTotalCedulas = (qtdeCedulas2*2) + (qtdeCedulas5*5) + \
+                (qtdeCedulas10*10) + (qtdeCedulas20*20)
+            totalCedulas = novoTotalCedulas
+            print(
+                f"A quantidade de cédulas de 2 reais é de: {qtdeCedulas2}")
+            print(
+                f"A quantidade de cédulas de 5 reais é de: {qtdeCedulas5}")
+            print(
+                f"A quantidade de cédulas de 10 reais é de: {qtdeCedulas10}")
+            print(
+                f"A quantidade de cédulas de 20 reais é de: {qtdeCedulas20}")
+            print(
+                f"O total de dinheiro em cedulas na máquina é de R${totalCedulas}")
+            # Solicita a quantidade de moedas de cada valor e atualiza o total em dinheiro da máquina
+            print("------------------------------------------------------------")
+            print("Digite a quantidade de moedas")
+            qtdeMoedas5 = int(
+                input("Digite a quantidade de moedas de 5 centavos: "))
+            qtdeMoedas10 = int(
+                input("Digite a quantidade de moedas de 10 centavos: "))
+            qtdeMoedas25 = int(
+                input("Digite a quantidade de moedas de 25 centavos: "))
+            qtdeMoedas50 = int(
+                input("Digite a quantidade de moedas de 50 centavos: "))
+            qtdeMoedas1 = int(
+                input("Digite a quantidade de moedas de 1 real: "))
+            novoTotalMoedas = ((qtdeMoedas5*5)/100) + ((qtdeMoedas10*10)/100) + \
+                ((qtdeMoedas25*25)/100) + \
+                ((qtdeMoedas50*50)/100) + (qtdeMoedas1*1)
+            totalMoedas = novoTotalMoedas
+            print(
+                f"A quantidade de moedas de 5 centavos é de: {qtdeMoedas5}")
+            print(
+                f"A quantidade de moedas de 10 centavos é de: {qtdeMoedas10}")
+            print(
+                f"A quantidade de moedas de 25 centavos é de: {qtdeMoedas25}")
+            print(
+                f"A quantidade de moedas de 50 centavos é de: {qtdeMoedas50}")
+            print(f"A quantidade de moedas de 1 real é de: {qtdeMoedas1}")
+            print(
+                f"O total de dinheiro em moedas na máquina é de R${totalMoedas:.2f}")
+            # Atualiza a quantidade de troco disponível na máquina
+            print("------------------------------------------------------------")
+            novo_troco = (totalCedulas + totalMoedas)
+            print(f"O total de troco na máquina é de R${novo_troco:.2f}")
+            troco = novo_troco
+            print("Troco atualizado com sucesso!")
+            print("------------------------------------------------------------")
+        # Se a opção selecionada for equivalente a 2, o estoque será atualizado por tipo e quantidade de produtos disponíveis.
+        elif opcaoAdm == "2":
+            print("------------------------------------------------------------")
+            print(
+                "A quantidade de unidade(s) de Coca-Cola atual em estoque é igual a: ", qtdeProduto1)
+            print("------------------------------------------------------------")
+            novaQtdeProduto1 = int(
+                input("Adicione mais unidade(s) de Coca-Cola ao estoque: "))
+            qtdeProduto1 = novaQtdeProduto1
+            print("Estoque atualizado com sucesso!")
+            print("------------------------------------------------------------")
+        # Verifica o valor recebido das vendas por produto e valor total durante o funcionamento da máquina
+        elif opcaoAdm == "3":
+            print("O valor recebido das vendas de Coca-Cola é igual a: R$",
+                  (qtdeProduto1Vendido*4))
+            print("O valor total das vendas é igual a: R$",
+                  (qtdeProduto1Vendido*4))
+        # Verifica se usuario selecionou uma opção válida de entrada, caso contrário retorna ao início do loop
+        else:
+            print("Opção inválida, tente novamente")
             continue
     # Se o usuário for consumidor habilita as funcionalidades para realizar as compras
     elif usuario == "2":
@@ -168,14 +168,14 @@ while True:
             elif totalRecebido > valorCompra:
                 troco = totalRecebido - valorCompra
                 print(
-                    f"Seu troco é de R${troco}, boa compra e receba suas {qtdeCompraProduto1} Coca-Colas")
+                    f"Seu troco é de R${troco}, boa compra e receba sua(s) {qtdeCompraProduto1} Coca-Cola(s)")
             elif totalRecebido == valorCompra:
                 print(
-                    f"Sem troco, boa compra e receba suas {qtdeCompraProduto1} Coca-Colas")
+                    f"Sem troco, boa compra e receba sua(s) {qtdeCompraProduto1} Coca-Cola(s)")
             else:
                 divida = (valorCompra-totalRecebido)
                 print(
-                    f"Falta R${divida} para finalizar a compra, insira mais cédulas")
+                    f"Não foi possível finalizar a compra, falta R${divida} para realizar a compra, receba seu dinheiro de volta e tente novamente")
         elif pagamento == "2":
             # Recebe os valores em moedas digitados pelo consumidor
             print("Digite a quantidade de moedas para o pagamento")
@@ -198,10 +198,10 @@ while True:
             elif totalRecebido > valorCompra:
                 troco = totalRecebido - valorCompra
                 print(
-                    f"Seu troco é de R${troco}, boa compra e receba suas {qtdeCompraProduto1} Coca-Colas")
+                    f"Seu troco é de R${troco}, boa compra e receba sua(s) {qtdeCompraProduto1} Coca-Cola(s)")
             elif totalRecebido == valorCompra:
                 print(
-                    f"Sem troco, boa compra e receba suas {qtdeCompraProduto1} Coca-Colas")
+                    f"Sem troco, boa compra e receba sua(s) {qtdeCompraProduto1} Coca-Cola(s)")
             else:
                 divida = (valorCompra-totalRecebido)
                 print(
@@ -235,10 +235,10 @@ while True:
             elif totalRecebido > valorCompra:
                 troco = totalRecebido - valorCompra
                 print(
-                    f"Seu troco é de R${troco}, boa compra e receba suas {qtdeCompraProduto1} Coca-Colas")
+                    f"Seu troco é de R${troco}, boa compra e receba sua(s) {qtdeCompraProduto1} Coca-Cola(s)")
             elif totalRecebido == valorCompra:
                 print(
-                    f"Sem troco, boa compra e receba suas {qtdeCompraProduto1} Coca-Colas")
+                    f"Sem troco, boa compra e receba sua(s) {qtdeCompraProduto1} Coca-Cola(s)")
             else:
                 divida = (valorCompra-totalRecebido)
                 print(
@@ -247,16 +247,5 @@ while True:
             print("Opção inválida, tente novamente")
             continue
     # Mostra mensagem de agradecimento, finaliza a compra e pede se o consumidor quer realizar outra ou encerra o programa
-    print("Obrigado por utilizar nossa máquina de refrigerantes!")
-    print("------------------------------------------------------------")
-
-    continuar = input("Deseja realizar outra operação? (1) SIM ou (2) NÃO: ")
-    if continuar == "1":
-        print("Ok, vamos fazer outra compra!")
-    elif continuar == "2":
-        print("Obrigado por utilizar nosso programa!")
-        break
-    # Verifica se a opção é válida
-    else:
-        print("Opção inválida. Digite (1) para continuar ou (2) para sair.")
+    print("Obrigado por utilizar nossa máquina de refrigerantes, você irá retornar ao menu principal!")
     print("------------------------------------------------------------")
