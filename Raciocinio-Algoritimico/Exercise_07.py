4a908e55f1faf3c648f0e481e22534c05e7bce45
Rafael Galafassi <93929600+Tagliassi@users.noreply.github.com>
2023-05-04 23:23:49 +0000

    RA

--- Raciocinio-Algoritimico/Exercise_07.py
+++ Raciocinio-Algoritimico/Exercise_07.py
@@ -1,4 +1,6 @@
 vetor = ["dado_posicao_0", "dado_posicao_1", "dado_posicao_2"]
 
+vetor.append("dado_posicao_3")
+
 for indice_atual in range(len(vetor)):
-    print(vetor(cont))
+    print(vetor(indice_atual))
