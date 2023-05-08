6ddc4aa6237cecd9c1fe60a4bf4586f3e616e7e6
Rafael Galafassi <93929600+Tagliassi@users.noreply.github.com>
2023-05-08 17:47:45 -0300

    Create Monitoria_05.py
    RA - Monitoria

---
+++ Raciocinio-Algoritimico/Monitoria_05.py
@@ -0,0 +1,32 @@
+#meu_dicionario = {"chave_1" : "item1", "chave_2" : "item2"}
+#meu_dicionario = {}
+
+#minha_tupla = ("item1","item2","item3")
+#minha_tupla = ()
+
+pokedex = {"GENGAR" :{
+    "TIPO" : "GHOST",
+    "ATAQUES" : ("SHADOW BALL", "LICK", "BITE")
+    },
+    "VOLTORB" :
+    {
+        "TIPO" : "ELETRICO",
+        "ATAQUES" :  ("THUNDEBOLT", "HEADBUTT", "SHOCKWAVE") 
+     },
+    "GROWLITHE" :
+    {
+        "TIPO" : ("FOGO", "NORMAL"),
+        "ATAQUES" :  ("CHARGE", "FLAMETHROWER", "SCRATCH") 
+     }
+
+    }
+
+comando = 0
+
+while comando != 9:
+    comando = input("Insira um nome de um pokemon para consultar suas informacoes \n ou insira 9 para encerrar: ")
+
+    if comando != 9:
+        print(pokedex.get(comando))
+    else:
+        print("Volte sempre")
