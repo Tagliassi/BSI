f02307ee75c76bc90dbb09c345baa12de74d590b
Tagliassi <rafael.galfassi2005@gmail.com>
2023-05-16 19:28:01 -0300

    RA

--- Raciocinio-Algoritimico/Exercise_09.py
+++ Raciocinio-Algoritimico/Exercise_09.py
@@ -3,8 +3,8 @@
 numero = input("Digite seu numero de celular: ")
 
 if numero[0] == 4 and numero[1] == 1:
-  print("É de Curitiba")
+    print("É de Curitiba")
 else:
-  print("Não é de Curitiba")
+    print("Não é de Curitiba")
 
 # Exercise 02:
