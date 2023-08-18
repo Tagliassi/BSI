6620702aabbe98662f3e11f4c98b7bc7060e5338
Tagliassi <rafael.galafassi2005@gmail.com>
2023-08-18 18:09:16 -0300

    POO

--- 2° Período/P.O.O/Exercise_01/src/ContaBanco.java
+++ 2° Período/P.O.O/Exercise_01/src/ContaBanco.java
@@ -6,36 +6,71 @@ public class ContaBanco {
     private double saldo;
     private boolean status;
 
+    public void statusAtual(){
+        System.out.println();
+        System.out.println("Conta:"+ this.getNumConta());
+        System.out.println("Tipo:"+ this.getTipo());
+        System.out.println("Dono:"+ this.getDono());
+        System.out.println("Saldo:"+ this.getSaldo());
+        System.out.println("Status da conta:"+ this.isStatus());
+    }
+
     public void abrirConta(String tipo) {
-        setTipo(tipo);
-        setStatus(true);
+        this.setTipo(tipo);
+        this.setStatus(true);
         if (tipo == "CC") {
-            setSaldo(50.0f);
+            this.setSaldo(50.0f);
         } else if (tipo == "CP") {
-            setSaldo(150.0f);
+            this.setSaldo(150.0f);
         } else
             System.out.println("ERRO! Escolha (CC) para conta corrente ou (CP) para conta poupança!");
     }
 
     public void fecharConta() {
-
+        if(this.getSaldo() > 0){
+            System.out.println("A sua conta possuí dinheiro");
+        }
+        else if(this.getSaldo() < 0){
+            System.out.println("A sua conta está em débito");
+        }
+        else{this.setStatus(false);}
     }
 
-    public void depositar() {
-
+    public void depositar(double valor) {
+        if(this.isStatus() == true){
+            this.setSaldo(this.getSaldo() + valor);
+        }
+        else {System.out.println("Conta fechada, impossível depositar!");}
     }
 
-    public void sacar() {
-
+    public void sacar(double valor) {
+        if(this.isStatus() == true){
+            if(this.getSaldo() >= valor) {
+                this.setSaldo(this.getSaldo() - valor);
+            }
+            else {System.out.println("Saldo insuficiente");}
+        }
+        else {System.out.println("Conta fechada, impossível sacar!");}
     }
 
     public void pagarMensal() {
-
+        double taxa = 0;
+        if(this.getTipo() == "CC"){
+            taxa = 12;
+        }
+        else {taxa = 20;}
+        if(this.isStatus() == true){
+            if(this.getSaldo() >= taxa) {
+                this.setSaldo(this.getSaldo() - taxa);
+            }
+            else {System.out.println("Saldo insuficiente");}
+        }
+        else {System.out.println("Conta fechada, impossível pagar mensalidade!");}
     }
 
     public ContaBanco() {
-        double saldo = 0;
-        boolean status = false;
+        this.saldo = 0;
+        this.status = false;
     }
 
     public String getDono() {
--- 2° Período/P.O.O/Exercise_01/src/Main.java
+++ 2° Período/P.O.O/Exercise_01/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Roberto");
+        p1.abrirConta("CC");
+        p1.depositar(100.00);
+
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(2222);
+        p2.setDono("Lara");
+        p2.abrirConta("CP");
+        p2.depositar(500.00);
+        p2.sacar(100.00);
+
+        p1.statusAtual();
+        p2.statusAtual();
+        
     }
 }
