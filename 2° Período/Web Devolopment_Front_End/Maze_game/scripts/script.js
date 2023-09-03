d7b5c1955e9b1c2843e4b7a51ca2ad2a3b6136f3
Tagliassi <rafael.galafassi2005@gmail.com>
2023-09-03 14:49:33 -0300

    WEB

--- 2° Período/Web Devolopment_Front_End/Maze_game/scripts/script.js
+++ 2° Período/Web Devolopment_Front_End/Maze_game/scripts/script.js
@@ -1,50 +1,58 @@
+// Classe Monstro
 class Monstro {
     constructor(nome, vida, dano, defesa, critico) {
-      this.nome = nome;
-      this.vida = vida;
-      this.dano = dano;
-      this.defesa = defesa; // 0.0 a 1.0
-      this.criticoChance = critico; 
+        this.nome = nome;
+        this.vida = vida;
+        this.dano = dano;
+        this.defesa = defesa; // Valor entre 0.0 e 1.0
+        this.criticoChance = critico;
     }
-  
+
     apresentar() {
-      console.log(`Olá, meu nome é ${this.nome} e sou um MONSTRO HORRÍVELll !!! MUAHUAHAUAHAHA`);
+        console.log(`Olá, meu nome é ${this.nome} e sou um MONSTRO HORRÍVELll !!! MUAHUAHAUAHAHA`);
     }
-  
+
     atacar(personagem) {
-      personagem.vida -= this.dano;
+        personagem.vida -= this.dano;
     }
-  
+
     realizarAtaqueCritico() {
-      let valorCritico = Math.random() < this.criticoChance ? this.dano * 2 : this.dano;
-      return valorCritico;
+        let valorCritico = Math.random() < this.criticoChance ? this.dano * 2 : this.dano;
+        return valorCritico;
     }
 }
-  
+
+// Classe Personagem
 class Personagem {
     constructor(nome) {
         this.nome = nome;
         this.vida = 100;
         this.dano = 10;
-        this.defesa = 0; 
+        this.defesa = 0;
         this.inventario = [];
-}
+    }
 
-apresentar() {
-    console.log(`Olá, meu nome é ${this.nome}`);
-}
+    apresentar() {
+        console.log(`Olá, meu nome é ${this.nome}`);
+    }
 
-atacar(monstro) {
-    let danoTotal = this.dano * (1 - monstro.defesa);
-    monstro.vida -= danoTotal;
-}
+    atacar(monstro) {
+        let danoTotal = this.dano * (1 - monstro.defesa);
+        monstro.vida -= danoTotal;
+    }
 
-realizarAtaqueCritico() {
-    let valorCritico = Math.random() < 0.7 ? this.dano * 2 : this.dano;
-    return valorCritico;
+    realizarAtaqueCritico() {
+        let valorCritico = Math.random() < 0.7 ? this.dano * 2 : this.dano;
+        return valorCritico;
+    }
+
+    // Método para adicionar um item ao inventário do jogador
+    adicionarItemInventario(item) {
+        this.inventario.push(item);
     }
 }
 
+// Classe Armadilha
 class Armadilha {
     constructor(nome, dano) {
         this.nome = nome;
@@ -56,6 +64,64 @@ class Armadilha {
     }
 }
 
+// Classe Localizacao
+class Localizacao {
+    constructor(name, descricao) {
+        this.nome = name;
+        this.descricao = descricao;
+        this.monstro = null;
+        this.evento = null;
+        this.item = null;
+    }
+
+    // Método para definir um monstro no local
+    setMonster(monster) {
+        this.monster = monster;
+    }
+
+    // Método para definir um evento no local
+    setEvent(evento) {
+        this.evento = evento;
+    }
+
+    // Método para definir um item no local
+    setItem(item) {
+        this.item = item;
+    }
+}
+
+// Classe Evento
+class Evento {
+    constructor(descricao) {
+        this.descricao = descricao;
+    }
+}
+
+// Classe GameMap
+class GameMap {
+    constructor() {
+        // Definir mapa bidimensional
+        this.map = [
+            [new Localizacao("Floresta", "Você está na floresta."), new Localizacao("Caverna", "Você está em uma caverna.")],
+            [new Localizacao("Montanha", "Você está na montanha."), new Localizacao("Cidade", "Você está na cidade.")],
+        ];
+        this.LocalizacaoAtual = this.map[0][0]; // Comece na primeira localização
+    }
+
+    // Método para mover o jogador para uma localização específica
+    movePersonagem(x, y) {
+        if (x >= 0 && x < this.map.length && y >= 0 && y < this.map[0].length) {
+            this.LocalizacaoAtual = this.map[x][y];
+        }
+    }
+}
+
+// Função para atualizar a interface do jogo
+function updateGameInterface() {
+    // Implemente a lógica para atualizar a interface do jogo aqui
+}
+
+// Classe Cura
 class Cura {
     constructor(nome, cura) {
         this.nome = nome;
@@ -67,6 +133,7 @@ class Cura {
     }
 }
 
+// Classe Arma
 class Arma {
     constructor(nome, dano) {
         this.nome = nome;
@@ -78,6 +145,7 @@ class Arma {
     }
 }
 
+// Classe Porta
 class Porta {
     constructor(nome, tipoDeChave) {
         this.nome = nome;
@@ -95,6 +163,7 @@ class Porta {
     }
 }
 
+// Classe Portal
 class Portal {
     constructor(nome) {
         this.nome = nome;
@@ -105,9 +174,24 @@ class Portal {
     }
 }
 
+// Classe Chave
 class Chave {
     constructor(tipoDeChave) {
         this.tipoDeChave = tipoDeChave;
     }
 }
-  
+
+// Função para iniciar o jogo
+function startGame() {
+    const playerName = prompt("Qual é o seu nome?");
+    const player = new Personagem(playerName);
+    const gameMap = new GameMap();
+
+    // Inicialize a interface do jogo
+    updateGameInterface();
+
+    // Implemente o loop do jogo aqui, permitindo que o jogador faça escolhas e avance no jogo
+}
+
+// Inicie o jogo quando a página for carregada
+window.onload = startGame;
