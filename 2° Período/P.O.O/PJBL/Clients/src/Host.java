0cca7bb8c5ffbf73e53cffc693acfecfcf66eb7e
Tagliassi <rafael.galafassi2005@gmail.com>
2023-10-05 19:52:33 -0300

    Creation of clients classes and methods

---
+++ 2° Período/P.O.O/PJBL/Clients/src/Host.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Host extends User {
+
+    public Host(String name, Date birDate, Contact contactInfo){
+        super(name, birDate, contactInfo);
+    }
+
+    public void createReservation(){
+
+    }
+}
---
+++ 2° Período/P.O.O/PJBL/Clients/src/Hotelier.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Hotelier extends User {
+
+    public Hotelier(String name, Date birDate, Contact contactInfo){
+        super(name, birDate, contactInfo);
+    }
+
+    public void createHotel(){
+        
+    }
+    
+}
