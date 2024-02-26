34d42f87a66ed01257a0b0f1743a02f7463a1e14
Tagliassi <rafael.galafassi2005@gmail.com>
2024-02-26 11:56:47 -0300

    Updates

--- 3° Período/Web-Development Framework/WebFrameworkApp/src/app/app.component.spec.ts
+++ 3° Período/Web-Development Framework/WebFrameworkApp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'webFramework' title`, () => {
+  it(`should have the 'WebFrameworkApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('webFramework');
+    expect(app.title).toEqual('WebFrameworkApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, webFramework');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, WebFrameworkApp');
   });
 });
