8f368772d988abf8a258b29e5b2a7b341dc67a89
Tagliassi <rafael.galafassi2005@gmail.com>
2024-02-29 22:35:28 -0300

    Functions exercises

---
+++ 3° Período/Web-Development-Canvas-Games/Aula01/Exercise.js
@@ -0,0 +1,67 @@
+const menos = (a,b) => b === undefined ? -a : a-b;
+
+const maior = (...nums) => {
+    let maior = nums[0];
+
+    for (n of nums){
+        n > maior ? (maior = n) : maior;
+    }
+
+    return maior;
+};
+
+const albuns = [
+    {nome: "Mais", cantor: "Marisa monte", ano: 1991, nota: 8.5},
+    {nome: "A tempestade", cantor: "Legião Urbana", ano: 1996, nota: 9.5},
+    {nome: "Domingo", cantor: "Titãs", ano: 1995, nota: 7.5},
+    {nome: "Ouro de Minas", cantor: "Roupa Nova", ano: 2001, nota: 8},
+    {nome: "Como estão vocês", cantor: "Titãs", ano: 2003, nota: 7},
+    {nome: "Troco Likes", cantor: "Thiago Iorc", ano: 2015, nota: 4.5},
+    {nome: "Dois", cantor: "Legião Urbana", ano: 1986, nota: 10},
+    {nome: "Radiola", cantor: "Skank", ano: 2004, nota: 6.5},
+    {nome: "Roupa acústico", cantor: "Roupa Nova", ano: 2004, nota: 9},
+    {nome: "Umbilical", cantor: "Thiago Iorc", ano: 2011, nota: 3.5},
+    {nome: "Barulhinho bom", cantor: "Marisa monte", ano: 1996, nota: 7.5},
+{nome: "Acústico MTV", cantor: "Capital Inicial", ano: 2000, nota: 8.5},
+{nome: "Admirável Chip Novo", cantor: "Pitty", ano: 2003, nota: 9},
+{nome: "Cê", cantor: "Caetano Veloso", ano: 2006, nota: 8},
+{nome: "Da Lama ao Caos", cantor: "Chico Science & Nação Zumbi", ano: 1994, nota: 9.5},
+{nome: "Do Cóccix Até o Pescoço", cantor: "Elza Soares", ano: 2002, nota: 9},
+{nome: "Equinócio", cantor: "Secos & Molhados", ano: 1973, nota: 8.5},
+{nome: "Iê Iê Iê", cantor: "Arnaldo Antunes", ano: 2009, nota: 7.5},
+{nome: "Krig-ha, Bandolo!", cantor: "Raul Seixas", ano: 1973, nota: 10},
+{nome: "Libra", cantor: "Zeca Baleiro", ano: 2003, nota: 8},
+{nome: "MTV ao Vivo", cantor: "Barão Vermelho", ano: 2005, nota: 8.5},
+{nome: "N", cantor: "Nando Reis", ano: 2010, nota: 9},
+{nome: "O Dia em Que Faremos Contato", cantor: "Lenine", ano: 1997, nota: 9},
+{nome: "Paula Toller", cantor: "Paula Toller", ano: 1998, nota: 8},
+{nome: "Que País É Este", cantor: "Legião Urbana", ano: 1987, nota: 9.5},
+{nome: "Racional", cantor: "Tim Maia", ano: 1975, nota: 10},
+{nome: "Sobrevivendo no Inferno", cantor: "Racionais MC's", ano: 1997, nota: 9.5},
+{nome: "Tribalistas", cantor: "Tribalistas", ano: 2002, nota: 8.5},
+{nome: "Verdade uma Ilusão", cantor: "Marisa Monte", ano: 2013, nota: 7.5},
+{nome: "Vô Imbolá", cantor: "Zeca Pagodinho", ano: 1996, nota: 8},
+{nome: "Zé Ramalho", cantor: "Zé Ramalho", ano: 1978, nota: 9}
+];
+
+function unicos(list, camp){
+
+    let obj = [];
+
+    list.forEach(element => {
+        const {[camp]: value} = element
+        obj.includes(value)?null:obj.push(value)
+    });
+
+    return obj
+}
+
+const cantores = unicos(albuns, 'cantor');
+
+function collatz(num){
+
+    while (num !== 0){
+        console.log(num)
+        num = num % 0 === 0 ? num / 2 : (3 * num + 1);
+    }
+}
