43f0ad123ece4208563eb1bee8e61b1bfd6a807e
Tagliassi <rafael.galafassi2005@gmail.com>
2024-03-07 22:47:43 -0300

    Exercises

--- 3° Período/Web-Development-Canvas-Games/RecapJS/Exercises_03.js
+++ 3° Período/Web-Development-Canvas-Games/RecapJS/Exercises_03.js
@@ -33,11 +33,138 @@ console.log(coordenadas.flatMap(c => Object.values(c)));
 const soma = [1, 2, 3, 4].reduce((a, e) => a + e);
 console.log("Soma:", soma);
 
-*/
-
 //Sort ordena a própria lista.
 let numeros = [1,2,3,4,10,9,8,7,6,5]
 
 console.log(numeros.sort((a,b) =>{
     return a-b;
 }))
+
+
+
+// Exercise 01
+
+const albuns = [
+    {nome: "Mais", cantor: "Marisa monte", ano: 1991, nota: 8.5},
+    {nome: "A tempestade", cantor: "Legião Urbana", ano: 1996, nota: 9.5},
+    {nome: "Domingo", cantor: "Titãs", ano: 1995, nota: 7.5},
+    {nome: "Ouro de Minas", cantor: "Roupa Nova", ano: 2001, nota: 8},
+    {nome: "Como estão vocês", cantor: "Titãs", ano: 2003, nota: 7},
+    {nome: "Troco Likes", cantor: "Thiago Iorc", ano: 2015, nota: 4.5},
+    {nome: "Dois", cantor: "Legião Urbana", ano: 1986, nota: 10},
+    {nome: "Radiola", cantor: "Skank", ano: 2004, nota: 6.5},
+    {nome: "Roupa acústico", cantor: "Roupa Nova", ano: 2004, nota: 9},
+    {nome: "Umbilical", cantor: "Thiago Iorc", ano: 2011, nota: 3.5},
+    {nome: "Barulhinho bom", cantor: "Marisa monte", ano: 1996, nota: 7.5},
+{nome: "Acústico MTV", cantor: "Capital Inicial", ano: 2000, nota: 8.5},
+{nome: "Admirável Chip Novo", cantor: "Pitty", ano: 2003, nota: 9},
+{nome: "Cê", cantor: "Caetano Veloso", ano: 2006, nota: 8},
+{nome: "Da Lama ao Caos", cantor: "Chico Science & Nação Zumbi", ano: 1994, nota: 9.5},
+{nome: "Do Cóccix Até o Pescoço", cantor: "Elza Soares", ano: 2002, nota: 9},
+{nome: "Equinócio", cantor: "Secos & Molhados", ano: 1973, nota: 8.5},
+{nome: "Iê Iê Iê", cantor: "Arnaldo Antunes", ano: 2009, nota: 7.5},
+{nome: "Krig-ha, Bandolo!", cantor: "Raul Seixas", ano: 1973, nota: 10},
+{nome: "Libra", cantor: "Zeca Baleiro", ano: 2003, nota: 8},
+{nome: "MTV ao Vivo", cantor: "Barão Vermelho", ano: 2005, nota: 8.5},
+{nome: "N", cantor: "Nando Reis", ano: 2010, nota: 9},
+{nome: "O Dia em Que Faremos Contato", cantor: "Lenine", ano: 1997, nota: 9},
+{nome: "Paula Toller", cantor: "Paula Toller", ano: 1998, nota: 8},
+{nome: "Que País É Este", cantor: "Legião Urbana", ano: 1987, nota: 9.5},
+{nome: "Racional", cantor: "Tim Maia", ano: 1975, nota: 10},
+{nome: "Sobrevivendo no Inferno", cantor: "Racionais MC's", ano: 1997, nota: 9.5},
+{nome: "Tribalistas", cantor: "Tribalistas", ano: 2002, nota: 8.5},
+{nome: "Verdade uma Ilusão", cantor: "Marisa Monte", ano: 2013, nota: 7.5},
+{nome: "Vô Imbolá", cantor: "Zeca Pagodinho", ano: 1996, nota: 8},
+{nome: "Zé Ramalho", cantor: "Zé Ramalho", ano: 1978, nota: 9}
+];
+
+marisa = []
+
+albuns.array.forEach(element => {
+    if(element.cantor == 'Marisa monte'){
+        marisa.push(element)
+    }
+});
+
+console.log(marisa)
+
+const monte = albuns.filter(a => a.cantor == "Marisa monte").map(({nome, ano}) => ({nome,ano}));
+
+console.log(monte)
+
+// Exercise 02
+
+// Exercise 03
+
+const albunsFiltrados = albuns.filter(album => album.ano<2005);
+
+const somaAlbuns = albunsFiltrados.reduce((total, {nota}) => total + nota, 0);
+const media_albuns = somaAlbuns / albunsFiltrados.length;
+console.log(media_albuns.toFixed(2));
+
+*/
+
+// Exercise 06
+
+const albuns = [
+    {nome: "Mais", cantor: "Marisa monte", ano: 1991, nota: 8.5},
+    {nome: "A tempestade", cantor: "Legião Urbana", ano: 1996, nota: 9.5},
+    {nome: "Domingo", cantor: "Titãs", ano: 1995, nota: 7.5},
+    {nome: "Ouro de Minas", cantor: "Roupa Nova", ano: 2001, nota: 8},
+    {nome: "Como estão vocês", cantor: "Titãs", ano: 2003, nota: 7},
+    {nome: "Troco Likes", cantor: "Thiago Iorc", ano: 2015, nota: 4.5},
+    {nome: "Dois", cantor: "Legião Urbana", ano: 1986, nota: 10},
+    {nome: "Radiola", cantor: "Skank", ano: 2004, nota: 6.5},
+    {nome: "Roupa acústico", cantor: "Roupa Nova", ano: 2004, nota: 9},
+    {nome: "Umbilical", cantor: "Thiago Iorc", ano: 2011, nota: 3.5},
+    {nome: "Barulhinho bom", cantor: "Marisa monte", ano: 1996, nota: 7.5},
+{nome: "Acústico MTV", cantor: "Capital Inicial", ano: 2000, nota: 8.5},
+{nome: "Admirável Chip Novo", cantor: "Pitty", ano: 2003, nota: 9},
+{nome: "Cê", cantor: "Caetano Veloso", ano: 2006, nota: 8},
+{nome: "Da Lama ao Caos", cantor: "Chico Science & Nação Zumbi", ano: 1994, nota: 9.5},
+{nome: "Do Cóccix Até o Pescoço", cantor: "Elza Soares", ano: 2002, nota: 9},
+{nome: "Equinócio", cantor: "Secos & Molhados", ano: 1973, nota: 8.5},
+{nome: "Iê Iê Iê", cantor: "Arnaldo Antunes", ano: 2009, nota: 7.5},
+{nome: "Krig-ha, Bandolo!", cantor: "Raul Seixas", ano: 1973, nota: 10},
+{nome: "Libra", cantor: "Zeca Baleiro", ano: 2003, nota: 8},
+{nome: "MTV ao Vivo", cantor: "Barão Vermelho", ano: 2005, nota: 8.5},
+{nome: "N", cantor: "Nando Reis", ano: 2010, nota: 9},
+{nome: "O Dia em Que Faremos Contato", cantor: "Lenine", ano: 1997, nota: 9},
+{nome: "Paula Toller", cantor: "Paula Toller", ano: 1998, nota: 8},
+{nome: "Que País É Este", cantor: "Legião Urbana", ano: 1987, nota: 9.5},
+{nome: "Racional", cantor: "Tim Maia", ano: 1975, nota: 10},
+{nome: "Sobrevivendo no Inferno", cantor: "Racionais MC's", ano: 1997, nota: 9.5},
+{nome: "Tribalistas", cantor: "Tribalistas", ano: 2002, nota: 8.5},
+{nome: "Verdade uma Ilusão", cantor: "Marisa Monte", ano: 2013, nota: 7.5},
+{nome: "Vô Imbolá", cantor: "Zeca Pagodinho", ano: 1996, nota: 8},
+{nome: "Zé Ramalho", cantor: "Zé Ramalho", ano: 1978, nota: 9}
+];
+
+const cantores = [
+    {nome: "Vinícius de Moraes", estilo: "MPB"},
+    {nome: "Rita Lee", estilo: "Rock"},
+    {nome: "Marisa monte", estilo: "MPB"},
+    {nome: "Legião Urbana", estilo: "Rock"},
+    {nome: "Titãs", estilo: "Rock"},
+    {nome: "Roupa Nova", estilo: "Pop rock"},
+    {nome: "Thiago Iorc", estilo: "Nova MPB"},
+    {nome: "Skank", estilo: "Pop rock"}
+];
+
+const listaSubstituida = albuns
+    .map(album => ({...album, cantor: cantores.filter(c => c.nome === album.cantor)[0] || album.cantor }))
+
+console.log(listaSubstituida)
+
+//other way...
+
+const mapCantores = {}
+
+cantores.forEach(c => mapCantores[c.nome] = c)
+
+console.log(mapCantores)
+
+const listaSubstituida2 = albuns
+    .map(album => ({...album, cantor: mapCantores[album.cantor] || album.cantor }))
+
+console.log(listaSubstituida2)
