5fb1e2c3e41171e1f672c5271a873bca7242cc39
Rafael Galafassi <Rafael.galafassi2005@gmail.com>
2025-02-25 21:40:39 -0300

    primeira aula

---
+++ 5° Período/Estrutura de Dados/Revisao_POO/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void  main(String[] args){
+
+        int opcao = 0;
+
+        System.out.println("Escolha uma opcao: ");
+        opcao = int(System.out.println("[0] Fazer Pedido"\n, "[1] Sair"));
+
+
+    }
+}
